21a6f381b8fd97c21f4d7beb19a6cf5b9e57fced
castilloglenn <castilloglenn@ymail.com>
2021-09-18 16:13:10 +0800

    added color definitions

--- src/utils/Gallery.java
+++ src/utils/Gallery.java
@@ -5,6 +5,12 @@ public class Gallery {
 	/**
 	 *  System theme color reference:
 	 *  https://coolors.co/45a3d9-edede9-051923-d84a31-ffffff
+	 *  
+	 *  Blue = (69, 163, 217)
+	 *  Gray = (237, 237, 233)
+	 *  Black = (5, 25, 35)
+	 *  Red = (216, 74, 49)
+	 *  White = (255, 255, 255)
 	 */
 
 }
