c6f460426104115fbf008f62d837952e3a2e497a
castilloglenn <castilloglenn@ymail.com>
2021-09-18 17:16:55 +0800

    added roundedpanel class for jpanel

--- src/inventory/Inventory.java
+++ src/inventory/Inventory.java
@@ -19,7 +19,8 @@ import javax.swing.border.LineBorder;
  */
 @SuppressWarnings("serial")
 public class Inventory extends JFrame {
-
+	
+	// private Gallery gallery; uncomment this after designing the UI
 	private JPanel mainPanel, navigationalPanel, dashboardPanel;
 
 	public static void main(String[] args) {
@@ -39,6 +40,13 @@ public class Inventory extends JFrame {
 	 * Create the frame.
 	 */
 	public Inventory() {
+		
+		// gallery = new Gallery();
+		// 			After designing, change all Panel to Rounded Panel like this:
+		// panelVariableExample = gallery.new RoundedPanel(Gallery.WHITE);
+		// 			And importantly set the opaque to false
+		// dashboardPanel.setOpaque(false);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1000, 600);
 		mainPanel = new JPanel();
@@ -54,11 +62,10 @@ public class Inventory extends JFrame {
 		sl_mainPanel.putConstraint(SpringLayout.WEST, navigationalPanel, 0, SpringLayout.WEST, mainPanel);
 		sl_mainPanel.putConstraint(SpringLayout.SOUTH, navigationalPanel, 0, SpringLayout.SOUTH, mainPanel);
 		sl_mainPanel.putConstraint(SpringLayout.EAST, navigationalPanel, 162, SpringLayout.WEST, mainPanel);
-		navigationalPanel.setBorder(new LineBorder(new Color(5, 25, 35), 0, true));
 		mainPanel.add(navigationalPanel);
 		
 		dashboardPanel = new JPanel();
-		dashboardPanel.setBackground(Gallery.GRAY);
+		dashboardPanel.setBackground(Gallery.WHITE);
 		sl_mainPanel.putConstraint(SpringLayout.NORTH, dashboardPanel, 15, SpringLayout.NORTH, mainPanel);
 		sl_mainPanel.putConstraint(SpringLayout.WEST, dashboardPanel, 15, SpringLayout.EAST, navigationalPanel);
 		sl_mainPanel.putConstraint(SpringLayout.SOUTH, dashboardPanel, -15, SpringLayout.SOUTH, mainPanel);
--- src/utils/Gallery.java
+++ src/utils/Gallery.java
@@ -1,6 +1,7 @@
 package utils;
 
-import java.awt.Color;
+import java.awt.*;
+import javax.swing.*;
 
 public class Gallery {
 	
@@ -22,5 +23,42 @@ public class Gallery {
 	public static Color WHITE = new Color(255, 255, 255);
 	
 	
+	@SuppressWarnings("serial")
+	public class RoundedPanel extends JPanel
+    {
+        private Color backgroundColor;
+        private int cornerRadius = 30;
+
+        public RoundedPanel(Color bgColor) {
+            super();
+            backgroundColor = bgColor;
+        }
+
+        public RoundedPanel(int radius, Color bgColor) {
+            super();
+            cornerRadius = radius;
+            backgroundColor = bgColor;
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Dimension arcs = new Dimension(cornerRadius, cornerRadius);
+            int width = getWidth();
+            int height = getHeight();
+            Graphics2D graphics = (Graphics2D) g;
+            graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            //Draws the rounded panel with borders.
+            if (backgroundColor != null) {
+                graphics.setColor(backgroundColor);
+            } else {
+                graphics.setColor(getBackground());
+            }
+            graphics.fillRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height); //paint background
+            graphics.setColor(getForeground());
+            graphics.drawRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height); //paint border
+        }
+    }
 
 }
