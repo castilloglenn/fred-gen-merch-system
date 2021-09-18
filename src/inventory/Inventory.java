ead30e7f905bccc11dc6ec2602b726a8441b4870
castilloglenn <castilloglenn@ymail.com>
2021-09-18 15:30:30 +0800

    restored to normal

--- src/inventory/Inventory.java
+++ src/inventory/Inventory.java
@@ -13,7 +13,7 @@ public class Inventory extends JFrame {
 	private JPanel contentPane;
 
 	/**
-	 * To be done by: Sebastian Gege
+	 * To be done by: Sebastian
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
