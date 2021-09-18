b1412feb0daaa4f8c6c478416e6be4fa0c4c1e4a
castilloglenn <castilloglenn@ymail.com>
2021-09-18 15:41:22 +0800

    added locationrelative

--- src/inventory/Inventory.java
+++ src/inventory/Inventory.java
@@ -10,7 +10,7 @@ import javax.swing.border.EmptyBorder;
 @SuppressWarnings("serial")
 public class Inventory extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel mainPanel;
 
 	/**
 	 * To be done by: Sebastian
@@ -34,10 +34,11 @@ public class Inventory extends JFrame {
 	public Inventory() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		mainPanel = new JPanel();
+		mainPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		mainPanel.setLayout(new BorderLayout(0, 0));
+		setContentPane(mainPanel);
+		setLocationRelativeTo(null); 
 	}
 
 }
