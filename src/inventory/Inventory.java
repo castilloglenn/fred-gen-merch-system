d5f8f0a632a07bfccb9ff34bba639c354d081493
castilloglenn <castilloglenn@ymail.com>
2021-09-18 16:46:19 +0800

    changes in inventory

--- src/inventory/Inventory.java
+++ src/inventory/Inventory.java
@@ -9,6 +9,9 @@ import javax.swing.border.EmptyBorder;
 
 import utils.Database;
 import utils.Gallery;
+import javax.swing.SpringLayout;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
 
 
 /**
@@ -17,7 +20,7 @@ import utils.Gallery;
 @SuppressWarnings("serial")
 public class Inventory extends JFrame {
 
-	private JPanel mainPanel;
+	private JPanel mainPanel,navigationalPanel;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -37,12 +40,29 @@ public class Inventory extends JFrame {
 	 */
 	public Inventory() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 689, 381);
+		setBounds(100, 100, 1000, 600);
 		mainPanel = new JPanel();
-		mainPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		mainPanel.setBackground(Gallery.BLACK);
 		setContentPane(mainPanel);
-		mainPanel.setLayout(null);
+		SpringLayout sl_mainPanel = new SpringLayout();
+		mainPanel.setLayout(sl_mainPanel);
 		setLocationRelativeTo(null); 
+		
+		navigationalPanel = new JPanel();
+		navigationalPanel.setBackground(Gallery.BLUE);
+		sl_mainPanel.putConstraint(SpringLayout.NORTH, navigationalPanel, 0, SpringLayout.NORTH, mainPanel);
+		sl_mainPanel.putConstraint(SpringLayout.WEST, navigationalPanel, 0, SpringLayout.WEST, mainPanel);
+		sl_mainPanel.putConstraint(SpringLayout.SOUTH, navigationalPanel, 0, SpringLayout.SOUTH, mainPanel);
+		sl_mainPanel.putConstraint(SpringLayout.EAST, navigationalPanel, 162, SpringLayout.WEST, mainPanel);
+		navigationalPanel.setBorder(new LineBorder(new Color(5, 25, 35), 0, true));
+		mainPanel.add(navigationalPanel);
+		
+		JPanel dashboardPanel = new JPanel();
+		sl_mainPanel.putConstraint(SpringLayout.NORTH, dashboardPanel, 15, SpringLayout.NORTH, mainPanel);
+		sl_mainPanel.putConstraint(SpringLayout.WEST, dashboardPanel, 15, SpringLayout.EAST, navigationalPanel);
+		sl_mainPanel.putConstraint(SpringLayout.SOUTH, dashboardPanel, -15, SpringLayout.SOUTH, mainPanel);
+		sl_mainPanel.putConstraint(SpringLayout.EAST, dashboardPanel, -15, SpringLayout.EAST, mainPanel);
+		mainPanel.add(dashboardPanel);
+		
 	}
-
 }
