676cf01605e2cb2e458c20d08105ce8ce9601de7
castilloglenn <castilloglenn@ymail.com>
2021-09-18 18:15:35 +0800

    added mouse hovering effect

--- src/inventory/Inventory.java
+++ src/inventory/Inventory.java
@@ -11,7 +11,14 @@ import utils.Database;
 import utils.Gallery;
 import javax.swing.SpringLayout;
 import java.awt.Color;
+import java.awt.Cursor;
+
 import javax.swing.border.LineBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
 
 
 /**
@@ -22,6 +29,9 @@ public class Inventory extends JFrame {
 	
 	// private Gallery gallery; uncomment this after designing the UI
 	private JPanel mainPanel, navigationalPanel, dashboardPanel;
+	private JLabel lblDashboard;
+	private JLabel lblSupplier;
+	private JLabel lblProduct;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -65,9 +75,61 @@ public class Inventory extends JFrame {
 		mainPanel.add(navigationalPanel);
 		
 		dashboardPanel = new JPanel();
-		dashboardPanel.setBackground(Gallery.WHITE);
+		dashboardPanel.setBackground(Gallery.GRAY);
 		sl_mainPanel.putConstraint(SpringLayout.NORTH, dashboardPanel, 15, SpringLayout.NORTH, mainPanel);
 		sl_mainPanel.putConstraint(SpringLayout.WEST, dashboardPanel, 15, SpringLayout.EAST, navigationalPanel);
+		SpringLayout sl_navigationalPanel = new SpringLayout();
+		navigationalPanel.setLayout(sl_navigationalPanel);
+		
+		lblDashboard = new JLabel("Dashboard");
+		sl_navigationalPanel.putConstraint(SpringLayout.NORTH, lblDashboard, 104, SpringLayout.NORTH, navigationalPanel);
+		sl_navigationalPanel.putConstraint(SpringLayout.WEST, lblDashboard, 10, SpringLayout.WEST, navigationalPanel);
+		sl_navigationalPanel.putConstraint(SpringLayout.SOUTH, lblDashboard, 138, SpringLayout.NORTH, navigationalPanel);
+		sl_navigationalPanel.putConstraint(SpringLayout.EAST, lblDashboard, -10, SpringLayout.EAST, navigationalPanel);
+		lblDashboard.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		lblDashboard.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				lblDashboard.setBackground(Gallery.WHITE);
+				lblDashboard.setOpaque(true);
+				lblDashboard.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				lblDashboard.setBackground(Gallery.BLUE);
+			}
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("ge");
+			}
+		});
+		lblDashboard.setHorizontalAlignment(SwingConstants.CENTER);
+		navigationalPanel.add(lblDashboard);
+		
+		lblSupplier = new JLabel("Supplier");
+		lblSupplier.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				lblSupplier.setBackground(Gallery.WHITE);
+				lblSupplier.setOpaque(true);
+			}
+		});
+		sl_navigationalPanel.putConstraint(SpringLayout.NORTH, lblSupplier, 20, SpringLayout.SOUTH, lblDashboard);
+		sl_navigationalPanel.putConstraint(SpringLayout.WEST, lblSupplier, 10, SpringLayout.WEST, navigationalPanel);
+		sl_navigationalPanel.putConstraint(SpringLayout.SOUTH, lblSupplier, -369, SpringLayout.SOUTH, navigationalPanel);
+		sl_navigationalPanel.putConstraint(SpringLayout.EAST, lblSupplier, 0, SpringLayout.EAST, lblDashboard);
+		lblSupplier.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSupplier.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		navigationalPanel.add(lblSupplier);
+		
+		lblProduct = new JLabel("Product");
+		sl_navigationalPanel.putConstraint(SpringLayout.NORTH, lblProduct, 26, SpringLayout.SOUTH, lblSupplier);
+		sl_navigationalPanel.putConstraint(SpringLayout.WEST, lblProduct, 10, SpringLayout.WEST, navigationalPanel);
+		sl_navigationalPanel.putConstraint(SpringLayout.SOUTH, lblProduct, -309, SpringLayout.SOUTH, navigationalPanel);
+		sl_navigationalPanel.putConstraint(SpringLayout.EAST, lblProduct, -10, SpringLayout.EAST, navigationalPanel);
+		lblProduct.setHorizontalAlignment(SwingConstants.CENTER);
+		lblProduct.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		navigationalPanel.add(lblProduct);
 		sl_mainPanel.putConstraint(SpringLayout.SOUTH, dashboardPanel, -15, SpringLayout.SOUTH, mainPanel);
 		sl_mainPanel.putConstraint(SpringLayout.EAST, dashboardPanel, -15, SpringLayout.EAST, mainPanel);
 		mainPanel.add(dashboardPanel);
