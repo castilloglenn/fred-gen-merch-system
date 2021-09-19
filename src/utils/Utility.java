008f0ea05616ed29096088177168c285772c0463
castilloglenn <castilloglenn@ymail.com>
2021-09-19 15:19:01 +0800

    separated roundedpanel and vertical label class

--- src/utils/Utility.java
+++ src/utils/Utility.java
@@ -2,7 +2,10 @@ package utils;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.AffineTransform;
+
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicLabelUI;
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -17,51 +20,9 @@ public class Utility {
 	public Font font;
 	private final String fontName = "OpenSans-SemiBold";
 	
-
-	@SuppressWarnings("serial")
-	public class RoundedPanel extends JPanel
-    {
-        private Color backgroundColor;
-        private int cornerRadius = 30;
-
-        public RoundedPanel(Color bgColor) {
-            super();
-            backgroundColor = bgColor;
-            setOpaque(false);
-        }
-
-        public RoundedPanel(int radius, Color bgColor) {
-            super();
-            cornerRadius = radius;
-            backgroundColor = bgColor;
-            setOpaque(false);
-        }
-
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            Dimension arcs = new Dimension(cornerRadius, cornerRadius);
-            int width = getWidth();
-            int height = getHeight();
-            Graphics2D graphics = (Graphics2D) g;
-            graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-            if (backgroundColor != null) {
-                graphics.setColor(backgroundColor);
-            } else {
-                graphics.setColor(getBackground());
-            }
-            graphics.fillRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);
-            graphics.setColor(getForeground());
-            graphics.drawRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);
-        }
-    }
-	
-	
 	public Utility() {
 		setupCustomFont();
 	}
-	
 	
 	public void setupCustomFont() {
 		try {
@@ -92,3 +53,4 @@ public class Utility {
 		return finalImage;
 	}
 }
+
